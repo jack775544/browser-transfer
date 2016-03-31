68361d81ce3f4160ca6cafd0d1ef77a8246c5dd2
jack775544 <jack775544@gmail.com>
2016-03-31 11:35:14 +1000

    Added in editor template

---
+++ edit.php
@@ -0,0 +1,83 @@
+<?php
+session_start();
+if ((!isset($_SESSION["username"])) || (!isset($_SESSION["password"])) || (!isset($_SESSION["remote"]))) {
+    unset($_SESSION["username"]);
+    unset($_SESSION["password"]);
+    unset($_SESSION["remote"]);
+    header("Location:index.php");
+}
+?>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Browser Transfer</title>
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!--<link rel="icon" href="http://getbootstrap.com/favicon.ico">-->
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap/bootstrap.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="bootstrap/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <link rel="stylesheet" href="css/filelist.css">
+</head>
+<body>
+<div class="row">
+    <div class="col-sm-11">
+        <h1>Browser Transfer</h1>
+    </div>
+    <div class="col-sm-1">
+        <form method="post" action="logout.php">
+            <input type="submit" name="logout" value="Logout" class="btn" id="logout">
+        </form>
+    </div>
+</div>
+<div class="jumbotron row files">
+    <div class="row">
+        <button type="button" id="refresh" class="btn"><span class="glyphicon glyphicon-refresh"
+                                                             aria-hidden="true"></span>Refresh
+        </button>
+        <button type="button" id="upload" class="btn"><span class="glyphicon glyphicon-open" aria-hidden="true"></span>Upload
+        </button>
+    </div>
+    <div class="row">
+        /home/students/s4356183
+    </div>
+</div>
+<div class="jumbotron files row">
+    <div class="row" id="editrow">
+        <pre id="editor">function foo(items) {
+    var i;
+    for (i = 0; i &lt; items.length; i++) {
+        alert("Ace Rocks " + items[i]);
+    }
+}</pre>
+    </div>
+</div>
+
+<script src="js/jquery.js"></script>
+<script src="bootstrap/bootstrap.js"></script>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="bootstrap/ie10-viewport-bug-workaround.js"></script>
+<script src="src-noconflict/ace.js" type="text/javascript" charset="utf-8"></script>
+<script>
+    var editor = ace.edit("editor");
+    editor.setTheme("ace/theme/twilight");
+    editor.session.setMode("ace/mode/javascript");
+</script>
+<script src="js/interface.js" type="text/javascript"></script>
+</body>
+</html>
